ab30560b2ab16a09654c750d5eb367e0d416b10d
Joachim Løvgaard <joachim@loevgaard.dk>
2023-03-01 15:09:56 +0100

    Add constructor to attribute

--- src/Attribute/Setting.php
+++ src/Attribute/Setting.php
@@ -7,12 +7,14 @@ namespace Setono\SettingsBundle\Attribute;
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 final class Setting
 {
-    public ?string $label = null;
-
-    public ?string $help = null;
-
-    public ?string $formType = null;
-
-    /** @var array<string, mixed> */
-    public array $formTypeOptions = [];
+    public function __construct(
+        public readonly ?string $label = null,
+        public readonly ?string $help = null,
+        public readonly ?string $formType = null,
+        /**
+         * @var array<string, mixed> $formTypeOptions
+         */
+        public readonly array $formTypeOptions = [],
+    ) {
+    }
 }
